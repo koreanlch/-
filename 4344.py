ebb5e86c4c02ed4233c87597b839c0fda9209e9e
koreanlch <49800235+koreanlch@users.noreply.github.com>
2022-01-25 17:04:31 +0900

    Create 4344.py

---
+++ 4344.py
@@ -0,0 +1,14 @@
+C = int(input())
+for i in range(C):
+    j =list(map(int,input().split()))
+    j.remove(j[0])
+    average = sum(j)/len(j)
+    def over(n):
+        if n > average:
+            return 0
+        else:
+            return 1
+    better = list(map(over,j))
+    ratio = better.count(0)/len(j)
+    answer = str(round(ratio*100,3)) + '%'
+    print(answer)
