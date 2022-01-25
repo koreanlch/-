c517ea541103f16b10a794a3655332bd7d7dc58e
koreanlch <49800235+koreanlch@users.noreply.github.com>
2022-01-25 15:13:34 +0900

    Create 1110.py

---
+++ 1110.py
@@ -0,0 +1,19 @@
+a = input()
+b = int(a)
+c = b//10
+d = b%10
+e = c+d
+new = int(str(d)+str(e%10)) 
+times = '*'
+
+while str(new) != a:
+    b,new = new,b
+    del(new,c,d,e)
+    c = b//10
+    d = b%10
+    e = c+d
+    new = int(str(d)+str(e%10))
+    times += '*'
+    print(c,d,e,new)
+
+print(str(int(times.count('*'))))
