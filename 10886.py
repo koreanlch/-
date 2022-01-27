0a7207efbfdfe12223677e59e8d7df2a36064081
koreanlch <49800235+koreanlch@users.noreply.github.com>
2022-01-28 04:47:33 +0900

    Create 10886.py

---
+++ 10886.py
@@ -0,0 +1,13 @@
+a = int(input())
+Y=0
+N=0
+for i in range(a):
+   o= int(input())
+   if o == 0:
+      N += 1
+   else:
+      Y += 1
+if Y>N :
+   print("Junhee is cute!")
+else:
+   print("Junhee is not cute!")
