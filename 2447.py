92f95b8a8777edc72fbe0dd00143f0e28e388744
koreanlch <49800235+koreanlch@users.noreply.github.com>
2022-01-31 17:38:25 +0900

    Create 2447.py

---
+++ 2447.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+N = int(sys.stdin.readline())
+k = int(math.log(N,3))
+def shape(x):
+    if x==1:
+        star = ["*"*3,"* *","*"*3]
+        return star
+    else:
+        star = [i*3 for i in shape(x-1)] + [i +" "*(3**(x-1))+i for i in shape(x-1)]+ [i*3 for i in shape(x-1)]
+        return star
+    
+    
+for j in shape(k):
+    print(j)
